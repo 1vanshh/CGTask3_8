3ade4275cebc2f4ad061cbddb8aa1c1c7720b53e
1vanshh <shamaev.ivan2005@gmail.com>
2024-11-12 17:17:13 +0300

    Creat project

---
+++ Task3_8/ru/vsu/cs/Affine/Scale.java
@@ -0,0 +1,24 @@
+package ru.vsu.cs.Affine;
+
+import ru.vsu.cs.interfaces.Scalable;
+
+public class Scale implements Scalable {
+
+    private Matrix4f scaleMatrix;
+    private Matrix4f initialMatrix;
+
+    @Override
+    public void scale(Vector3f scaleVector) {
+
+    }
+
+    @Override
+    public void applyScaling() {
+        
+    }
+
+    @Override
+    public void resetScaling() {
+
+    }
+}
