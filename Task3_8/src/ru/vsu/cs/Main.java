a56235d95003a406e308501371626da7da30c457
1vanshh <shamaev.ivan2005@gmail.com>
2024-12-13 21:44:49 +0300

    The architecture has been changed

--- Task3_8/src/ru/vsu/cs/Main.java
+++ Task3_8/src/ru/vsu/cs/Main.java
@@ -11,34 +11,6 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Main {
-
-//    public static Vec3f transform(Vec3f f, IAffine affine) {
-//        Vec3f a = Multiply.multiply(f, affine.getMatrix());
-//        return a;
-//    }
     public static void main(String[] args) {
-
-        Scale scale = new Scale(2, 2, 2);
-//        Rotate rotate = new Rotate(90, false, false, true, false);
-        Translation translation = new Translation(0, 0, 1);
-
-        SubAffine subAffine = new SubAffine();
-        subAffine.add(scale);
-//        subAffine.add(rotate);
-        subAffine.add(translation);
-
-        System.out.println(subAffine.getMatrix().toString());
-        /*List<Vector4f> lll = new ArrayList<>();
-
-        List<Vector4f> qq = subAffine.transformation(lll);
-        for (Vector4f v : qq) {
-            //.....
-        }
-
-        Iterator<Vector4f> itr = qq.iterator();
-        while(itr.hasNext()) {
-            Vector4f v = itr.next();
-            //....
-        }*/
     }
 }
